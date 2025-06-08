6680e628e61001677efd04a4d3ba2b5542fff010
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2025-06-08 13:33:05 -0500

    etities barrel file was created

---
+++ lib/features/products/domain/entities/entities.dart
@@ -0,0 +1,6 @@
+//This is a barrel file, allowing for a single import to the locations where the information is needed, making it more streamlined and organized.
+
+export 'package:flutter/foundation.dart';
+export 'package:tecnical_flutter_test_grupo_exito/features/products/domain/entities/product.dart';
+export 'package:tecnical_flutter_test_grupo_exito/features/products/domain/entities/shopping_cart.dart';
+
--- lib/features/products/domain/repository/products_repository.dart
+++ lib/features/products/domain/repository/products_repository.dart
@@ -1,9 +1,8 @@
 
 import 'package:dartz/dartz.dart';
 import 'package:tecnical_flutter_test_grupo_exito/core/error/failures.dart';
-import 'package:tecnical_flutter_test_grupo_exito/features/products/domain/entities/category.dart';
-import 'package:tecnical_flutter_test_grupo_exito/features/products/domain/entities/product.dart';
-import 'package:tecnical_flutter_test_grupo_exito/features/products/domain/entities/shopping_cart.dart';
+import 'package:tecnical_flutter_test_grupo_exito/features/products/domain/entities/entities.dart';
+
 
 abstract class ProductsRepository {
 
